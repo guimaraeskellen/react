43c2cfbb87909cd1b099259d70c1de1b92802cda
Kellen Guimaraes <kellen.adriely@gmail.com>
2020-05-24 22:31:24 -0400

    getting data from an API

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
 import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
 
 function App(props) {
+  const [ usuario, setUsuario ] = useState('');
+
+  function handlePesquisa(){
+    axios.get(`https://api.github.com/users/${usuario}/repos`).then(response => console.log(response.data));
+    
+  }
+
 
   return (
     <>
       <h1> { props.title } {props.user}</h1>
-      <input name="usuario" id="usuario" className="usuarioInput " placeholder="Usuário"/>
-      <button type="button>"> Pesquisar </button>
+      <h1> </h1>
+      <input className="usuario" placeholder="Usuário" value={usuario} onChange = { e => setUsuario(e.target.value) }/>
+      <button type="button>" onClick={handlePesquisa}>  Pesquisar </button>
     </>
   );
 }
