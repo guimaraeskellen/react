3e68ea2c81d8822a10eb65f39db9c4de585e5bac
Kellen Guimaraes <kellen.adriely@gmail.com>
2020-05-31 16:35:21 -0400

    conditional rendering

--- src/routes.js
+++ src/routes.js
@@ -1,13 +1,16 @@
 import React from 'react';
-
 import { Switch, Route, BrowserRouter } from 'react-router-dom';
-import Repositories from './Repositories';
+
+import Repositories from './pages/Repositories';
+import Home from './pages/Home';
+
 
 export default function Routes(){
     return (
         <BrowserRouter>
             <Switch>
                 <Route path='/repositories' component={Repositories}/>
+                <Route path= '/' exact component={Home}/>
             </Switch>       
         
         </BrowserRouter>
